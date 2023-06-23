969980c4ffb7dde4d1084d7ac07ef2572f6a7edd
SX <50966843+S-S-X@users.noreply.github.com>
2023-06-24 02:18:12 +0300

    Synchronize multiple branches

--- mirror.sh
+++ mirror.sh
@@ -3,22 +3,37 @@
 # full github url
 GITHUB_REPO_URL="https://${access_token}@github.com/${GITHUB_REPOSITORY_OWNER}/${github_repo}"
 
-# fetch commit-checksums
-LOCAL_SHA=$(git ls-remote "${GITHUB_REPO_URL}" ${branch} | cut -f1)
-REMOTE_SHA=$(git ls-remote "${source_repo}" ${branch} | head -n1 | cut -f1)
+# break down branch list
+ALL_BRANCHES=(${branch//,/ })
+BRANCHES=()
 
-echo "Local sha: ${LOCAL_SHA} (${github_repo})"
-echo "Remote sha: ${REMOTE_SHA} (${source_repo})"
+for branch_name in "${ALL_BRANCHES[@]}"; do
+    # fetch commit-checksums
+    LOCAL_SHA=$(git ls-remote "${GITHUB_REPO_URL}" ${branch_name} | cut -f1)
+    REMOTE_SHA=$(git ls-remote "${source_repo}" ${branch_name} | head -n1 | cut -f1)
 
-# exit if commits are equal
-test "${LOCAL_SHA}" == "${REMOTE_SHA}" && exit 0
+    echo "Local sha: ${LOCAL_SHA} (${github_repo})"
+    echo "Remote sha: ${REMOTE_SHA} (${source_repo})"
 
-echo "Changes detected! Cloning and updating local repo"
+    # add to list if commits are not equal
+    if [[ "${LOCAL_SHA}" != "${REMOTE_SHA}" ]]; then
+        echo "Changes detected in '${branch_name}' branch."
+        BRANCHES+=("${branch_name}")
+    fi
+done
 
-# clone, pull and push
+# bail out without failure if list is empty
+((${#BRANCHES[@]} > 0)) || exit 0
+
+# clone (use first branch in cloning)
 rm gh_repo -rf
-git clone -b ${branch} ${GITHUB_REPO_URL} gh_repo || exit 1
+git clone -b "${BRANCHES[0]}" "${GITHUB_REPO_URL}" gh_repo || exit 1
 cd gh_repo
-git pull ${source_repo} ${branch} || exit 1
-git push origin ${branch} || exit 1
 
+# pull and push all branches
+for branch_name in "${BRANCHES[@]}"; do
+    echo "Updating branch '${branch_name}'"
+    git checkout -b "${branch_name}"
+    git pull "${source_repo}" "${branch_name}" || exit 1
+    git push origin "${branch_name}" || exit 1
+done
